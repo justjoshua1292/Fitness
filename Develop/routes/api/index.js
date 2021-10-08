778fc3ad28d0435ec63d9fac73b1ef4b6989cc07
Joshua Harris <Justjoshua1292@gmail.com>
2021-10-08 19:42:41 -0400

    added models folder as well as an API routes folder

---
+++ Develop/routes/api/index.js
@@ -0,0 +1,13 @@
+const router = require("express").Router()
+const path = require("path");
+router.get("/", (req, res) => {
+
+})
+router.get("/stats", (req, res) => {
+
+})
+router.get("/exercise", (req, res) => {
+    
+})
+
+module.exports=router
